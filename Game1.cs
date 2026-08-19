f90f8b8bf008ce2e9ae006ff38bd3a2a3c2e9ced
lyiar_cs <liarley.personal@outlook.com>
2026-08-19 18:14:31 -0300

    update: drawing the ground, implementing the pipes (part 3)

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -10,13 +12,29 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
 
     private Texture2D _texture;
-    
-    private float _bgScrollSpeed = 1f;
+
+    private float _bgScrollSpeed = 0.5f;
+    private float _groundScrollSpeed = 2f;
 
     private Rectangle _bgSrcRect = new Rectangle(0, 0, 288, 512);
 
     private Vector2 _bgPos1;
     private Vector2 _bgPos2 = new Vector2(288f, 0f);
+
+    private Rectangle _groundSrcRect = new Rectangle(584, 0, 336, 112);
+    private Vector2 _groundPos1;
+    private Vector2 _groundPos2 = new Vector2(336f, 0f);
+
+    private List<Pipe> _pipes = [];
+    private Vector2 _initialPipesPosition;
+
+    private float _pipesSpawnInterval = 1.5f;
+    private float _pipesSpawnElapsed;
+    private float _maxDelta = 1f / 20f;
+
+    private float _minPipeY = 130f;
+    private float _maxPipeY = 290f;
+    private MathHelper.Random _rng = new MathHelper.Random();
 
     public Game1()
     {
@@ -30,6 +48,15 @@ public class Game1 : Game
         _graphics.PreferredBackBufferWidth = 288;
         _graphics.PreferredBackBufferHeight = 512;
         _graphics.ApplyChanges();
+
+        var windowHeight = _graphics.PreferredBackBufferHeight;
+        _groundPos1.Y = windowHeight - _groundSrcRect.Height;
+        _groundPos2.Y = _groundPos1.Y;
+
+        _initialPipesPosition = new Vector2(
+            _graphics.PreferredBackBufferWidth + 30f, // window width + 30px 
+            _graphics.PreferredBackBufferHeight / 2f
+        );
 
         base.Initialize();
     }
@@ -46,16 +73,80 @@ public class Game1 : Game
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-         _bgPos1.X -= _bgScrollSpeed;
-         if (_bgPos1.X + _bgSrcRect.Width < 0f)
-             _bgPos1.X = 0f;
+        ScrollBackground();
+        ScrollGround();
 
-         var windowWidth = _graphics.PreferredBackBufferWidth;
-         _bgPos2.X -= _bgScrollSpeed;
-         if (_bgPos2.X + _bgSrcRect.Width < windowWidth)
-             _bgPos2.X = windowWidth;
+        HandlePipesSpawning(gameTime);
+        ScrollPipes();
+        DestroyPipesOutsideScreen();
 
         base.Update(gameTime);
+    }
+
+    private void ScrollBackground()
+    {
+        _bgPos1.X -= _bgScrollSpeed;
+        if (_bgPos1.X + _bgSrcRect.Width < 0f)
+            _bgPos1.X = 0f;
+
+        _bgPos2.X -= _bgScrollSpeed;
+        var windowWidth = _graphics.PreferredBackBufferWidth;
+        if (_bgPos2.X + _bgSrcRect.Width < windowWidth)
+            _bgPos2.X = windowWidth;
+    }
+
+    private void ScrollGround()
+    {
+        _groundPos1.X -= _groundScrollSpeed;
+        if (_groundPos1.X + _groundSrcRect.Width < 0f)
+            _groundPos1.X = 0f;
+
+        _groundPos2.X -= _groundScrollSpeed;
+        var windowWidth = _graphics.PreferredBackBufferWidth;
+        if (_groundPos2.X + _groundSrcRect.Width < windowWidth)
+            _groundPos2.X = windowWidth;
+    }
+
+    private void HandlePipesSpawning(GameTime gameTime)
+    {
+        float delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        float frameTime = MathF.Min(_maxDelta, delta);
+        _pipesSpawnElapsed += frameTime;
+        if (_pipesSpawnElapsed >= _pipesSpawnInterval)
+        {
+            CreatePipe();
+            _pipesSpawnElapsed -= _pipesSpawnInterval;
+        }
+    }
+
+    private void CreatePipe()
+    {
+        var position = new Vector2(
+            _initialPipesPosition.X,
+            _rng.NextFloat(_minPipeY, _maxPipeY)
+        );
+        var pipe = new Pipe(_texture, position);
+        _pipes.Add(pipe);
+    }
+
+    private void ScrollPipes()
+    {
+        foreach (var pipe in _pipes)
+        {
+            var position = pipe.Position;
+            position.X -= _groundScrollSpeed;
+            pipe.Position = position;
+        }
+    }
+
+    private void DestroyPipesOutsideScreen()
+    {
+        for (int i = 0; i < _pipes.Count; i++)
+        {
+            var pipe = _pipes[i];
+            if (pipe.Position.X + 30f < 0f)
+                _pipes.RemoveAt(i);
+        }
     }
 
     protected override void Draw(GameTime gameTime)
@@ -65,6 +156,13 @@ public class Game1 : Game
         _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
         _spriteBatch.Draw(_texture, _bgPos1, _bgSrcRect, Color.White);
         _spriteBatch.Draw(_texture, _bgPos2, _bgSrcRect, Color.White);
+
+        foreach (var pipe in _pipes)
+            pipe.Draw(_spriteBatch);
+
+        _spriteBatch.Draw(_texture, _groundPos1, _groundSrcRect, Color.White);
+        _spriteBatch.Draw(_texture, _groundPos2, _groundSrcRect, Color.White);
+
         _spriteBatch.End();
 
         base.Draw(gameTime);
@@ -73,6 +171,7 @@ public class Game1 : Game
     protected override void Dispose(bool disposing)
     {
         _texture.Dispose();
+        _spriteBatch.Dispose();
         base.Dispose(disposing);
     }
 }
---
+++ Pipe.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FlappyBirdCourse;
+
+public class Pipe
+{
+    private readonly Texture2D _texture;
+    private Vector2 _position;
+    private Rectangle _topPipeSrcRect = new Rectangle(112, 646, 52, 320);
+    private Rectangle _bottomPipeSrcRect = new Rectangle(168, 646, 52, 320);
+
+    public Vector2 TopPipePosition { get; private set; }
+    public Vector2 BottomPipePosition { get; private set; }
+
+    public Vector2 Position
+    {
+        get => _position;
+        set
+        {
+            float halfPipeWidth = _topPipeSrcRect.Width / 2f;
+            float pipeHeight = _topPipeSrcRect.Height;
+            float halfGap = GapBetweenPipes / 2f;
+
+            TopPipePosition = new Vector2(value.X - halfPipeWidth, value.Y - pipeHeight - halfGap);
+            BottomPipePosition = new Vector2(value.X - halfPipeWidth, value.Y + halfGap);
+
+            _position = value;
+        }
+    }
+
+    public float GapBetweenPipes = 90f;
+
+    public Pipe(Texture2D texture, Vector2 position)
+    {
+        _texture = texture;
+        Position = position;
+    }
+
+    public void Draw(SpriteBatch batch)
+    {
+        batch.Draw(_texture, TopPipePosition, _topPipeSrcRect, Color.White);
+        batch.Draw(_texture, BottomPipePosition, _bottomPipeSrcRect, Color.White);
+    }
+}
